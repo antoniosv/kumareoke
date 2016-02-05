aa5ace4d860c1a107a11163919762af2f4bfa464
Antoniosv <jesus.antoniosv@gmail.com>
2016-02-05 14:48:56 -0600

    Now supports multiple formats to export

--- setup.py
+++ setup.py
@@ -5,14 +5,15 @@ import os
 import sys
 
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 60 * 1024 * 1024
 
-ALLOWED_EXTENSIONS = set(['.mp3', '.wav'])
+ALLOWED_EXTENSIONS = set(['.mp3', '.wav', '.ogg', '.flac'])
 
 def allowed_file(filename):
     return '.' in filename and os.path.splitext(filename)[1] in ALLOWED_EXTENSIONS
 
-def strip_vocals(wvocals):  
-    proc = Popen(["sox", "-t", ".mp3", "-", "-t", ".mp3", "-", "oops"], stdin=PIPE, stdout=PIPE, stderr=PIPE)    
+def strip_vocals(wvocals, ext):  
+    proc = Popen(["sox", "-t", ext, "-", "-t", ext, "-", "oops"], stdin=PIPE, stdout=PIPE, stderr=PIPE)    
     karaoke, err = proc.communicate(wvocals)
     ret_code = proc.wait()
 
@@ -29,8 +30,9 @@ def upload_song(name=None):
         f = request.files['full_track']
         if f and allowed_file(f.filename):
             filename = secure_filename(f.filename)
-            fkaraoke = (os.path.splitext(f.filename)[0] + ' (karaoke).mp3').encode('utf-8')
-            instrumental = strip_vocals(f.read())
+            extension = os.path.splitext(f.filename)[1]
+            fkaraoke = (os.path.splitext(f.filename)[0] + ' (karaoke)' + extension).encode('utf-8')
+            instrumental = strip_vocals(f.read(), extension)
             response = make_response(instrumental)
             response.headers['Content-Type'] = "application/octet-stream"
             response.headers["Content-Disposition"] = "attachment; filename='" + fkaraoke + "'"
@@ -38,7 +40,7 @@ def upload_song(name=None):
 #            return render_template('songupload.html', name=instrumental.filename)
         else:
             return 'Error with file uploaded. Check extension.'
-    return render_template('songupload.html')
+    return render_template('songupload.html', extensions=ALLOWED_EXTENSIONS)
 
 if __name__ == '__main__':
     # host parameter grants access to other nodes in the network
