9fab05df40c5ef9a63daee91f206c7ce4570b430
Antoniosv <jesus.antoniosv@gmail.com>
2016-02-08 11:22:22 -0600

    fixes previous build by adding config file

---
+++ config.py
@@ -0,0 +1,9 @@
+###
+### CONFIGURATION FILE FOR PROJECT
+###
+
+DATABASE = '/tmp/kumareoke.db'
+DEBUG = True
+SECRET_KEY = 'lordhighlordofthefairies'
+PASSPHRASE = ''
+MAX_CONTENT_LENGTH = 60 * 1024 * 1024
